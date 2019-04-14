8e4be6c5a521736f2a3b61c048ed470e5b514597
nikolas <nikolas@nikolass-mbp>
2019-04-14 16:31:18 +0300

    Revert "Moved code in src folder, adjusted scriptsa"
    This reverts commit ba97a3b25dc40f06e99ca5e042f26604e7f3e522.

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,25 +1,20 @@
 module.exports = {
     staticFileGlobs: [
-        'src/index.html',
-        'src/icons/**/*',
-        'src/views/**/*',
-        'src/component/**/*',
-        'src/manifest.json',
-        'src/lit-element-bootstrap-docs.js',
-        'node_modules/lit-html/**/*',
-        'node_modules/lit-element/**/*',
-        'node_modules/pwa-helpers/**/*',
-        'node_modules/popper.js/dist/esm/**/*',
-        'node_modules/lit-element-bootstrap/**/*',
-        'node_modules/@webcomponents/webcomponentsjs/**',
-        'node_modules/prismjs/components/prism-core.min.js',
-        'node_modules/prismjs/components/prism-markup.min.js',
-        'node_modules/prismjs/plugins/normalize-whitespace/prism-normalize-whitespace.min.js'
+        '/index.html',
+        '/manifest.json',
+        '/icons/**/*',
+        '/views/**/*',
+        '/component/**/*',
+        '/node_modules/lit-html/**/*',
+        '/node_modules/lit-element/**/*',
+        '/node_modules/pwa-helpers/**/*',
+        '/node_modules/popper.js/dist/esm/**/*',
+        '/node_modules/lit-element-bootstrap/**/*',
+        '/node_modules/@webcomponents/webcomponentsjs/**',
+        '/node_modules/prismjs/components/prism-core.min.js',
+        './node_modules/prismjs/components/prism-markup.min.js',
+        './node_modules/prismjs/plugins/normalize-whitespace/prism-normalize-whitespace.min.js'
     ],
-    "runtimeCaching": [{
-        "urlPattern": "/",
-        "handler": "networkFirst"
-    }],
-    navigateFallback: 'src/index.html',
+    navigateFallback: '/index.html',
     navigateFallbackWhitelist: [/^(?!.*\.js$|\/data\/).*/]
-}
+  }
