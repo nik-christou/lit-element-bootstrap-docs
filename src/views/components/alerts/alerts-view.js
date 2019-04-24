4dfc6079c20ce951c2687a1a3c8ea8fd872c4864
nikolas <nikolas@nikolas-All-Series>
2019-04-25 01:09:36 +0300

    Reverted alerts view, added content in getting started view

--- src/views/components/alerts/alerts-view.js
+++ src/views/components/alerts/alerts-view.js
@@ -1,17 +1,7 @@
 
 import { LitElement, html } from 'lit-element';
-import { unsafeHTML } from 'lit-html/directives/unsafe-html.js';
-import { TableOfContentMixin } from '../../../component/toc/table-of-content-mixin';
 import { BsDocsContentCss } from '../../../component/content/bs-docs-content-css';
 import { BsContentRebootCss, BsContentCodeCss, BsContentTypographyCss } from 'lit-element-bootstrap/content';
-import { PrismDefaultCss } from '../../../component/example/prism-default-css';
-
-import 'lit-element-bootstrap/layout/grid';
-import 'lit-element-bootstrap/components/navs';
-
-import '../../../component/example/bs-highlight';
-import '../../../component/toc/table-of-content';
-import '../../../component/toc/table-of-content-item';
 
 import './examples/contextual-alerts-example';
 import './examples/link-color-example';
@@ -19,90 +9,40 @@ import './examples/additional-content-example';
 import './examples/dismissing-alert-example';
 
 
-class AlertsView extends TableOfContentMixin(LitElement) {
+class AlertsView extends LitElement {
     
     static get styles() {
         return [
             BsContentRebootCss,
             BsContentTypographyCss,
             BsDocsContentCss,
-            BsContentCodeCss,
-            PrismDefaultCss
+            BsContentCodeCss
         ];
     }
     
     render() {
         return html`
-          
-            <bs-row>
-                <bs-column md-9 order-md-first order-xs-last>
+        
+            <h1 class="bd-title" id="content">Alerts</h1>
 
-                    <h1 class="bd-title" id="content">Alerts</h1>
-
-                    <p class="bd-lead">Provide contextual feedback messages for typical user actions with the handful of available and flexible alert messages.</p>
-                    
-                    <br />
-
-                    <h3>Usage</h3>
-                    
-                    <p>Import all alert components:</p>
-                    <bs-highlight>
-                        <pre><code id="importAllHighlight">${unsafeHTML(
-                        this._hightlightJavascript(
-                            `import 'lit-element-bootstrap/components/alert';`
-                        ))}</code></pre>
-                    </bs-highlight>
-
-                    <p>Import individually:</p>
-                    <bs-highlight>
-                        <pre><code id="importAllHighlight">${unsafeHTML(
-                            this._hightlightJavascript(
-                                `import 'lit-element-bootstrap/components/alert/bs-alert';`
-                            ))}</code></pre>
-                    </bs-highlight>
-                    
-                    <contextual-alerts-example></contextual-alerts-example>
-                
-                    <h2>Link Color</h2>
-                    
-                    <link-color-example></link-color-example>
-                    
-                    <h2>Additional Content</h2>
-                    
-                    <additional-content-example></additional-content-example>
-                    
-                    <h2>Dismissing</h2>
-                    
-                    <dismissing-alert-example></dismissing-alert-example>
-
-                    <br />
-                    <br />
-                    <br />
-
-                </bs-column>
-                <bs-column md-3 order-md-last order-xs-first>
-                    <table-of-content>
-                        <table-of-content-item element-id="usage" text="Usage"></table-of-content-item>
-                        <table-of-content-item element-id="attributes" text="Attributes"></table-of-content-item>
-                        <table-of-content-item element-id="properties" text="Properties"></table-of-content-item>
-                        <table-of-content-item element-id="slots" text="Slots"></table-of-content-item>
-                        <table-of-content-item element-id="css-properties" text="CSS Custom Properties"></table-of-content-item>
-                        <table-of-content-item element-id="events" text="Events"></table-of-content-item>
-                        <table-of-content-item element-id="examples" text="Examples"></table-of-content-item>
-                    </table-of-content>
-                </bs-column>
-            </bs-row>
+            <p class="bd-lead">Provide contextual feedback messages for typical user actions with the handful of available and flexible alert messages.</p>
+            
+            <br />
+            
+            <contextual-alerts-example></contextual-alerts-example>
+        
+            <h2>Link Color</h2>
+            
+            <link-color-example></link-color-example>
+            
+            <h2>Additional Content</h2>
+            
+            <additional-content-example></additional-content-example>
+            
+            <h2>Dismissing</h2>
+            
+            <dismissing-alert-example></dismissing-alert-example>
         `;
-    }
-
-    _hightlightJavascript(text) {
-
-        const grammar = window.Prism.languages.javascript;
-        const language = 'javascript';
-        const normilazedText = window.Prism.plugins.NormalizeWhitespace.normalize(text);
-        const hightlightedText = window.Prism.highlight(normilazedText, grammar, language);
-
-        return hightlightedText;
     }
 };
 
--- src/views/getting-started/getting-started-view.js
+++ src/views/getting-started/getting-started-view.js
@@ -1,7 +1,12 @@
 
-import { LitElement, html } from 'lit-element';
+import { LitElement, html,css } from 'lit-element';
+import { unsafeHTML } from 'lit-html/directives/unsafe-html.js';
 import { BsContentRebootCss, BsContentTypographyCss } from 'lit-element-bootstrap/content';
+import { BsDocsContentCss } from '../../component/content/bs-docs-content-css';
 import { BsFlexOrderCss } from 'lit-element-bootstrap/utilities/flex/bs-flex-order-css';
+import { PrismDefaultCss } from '../../component/example/prism-default-css';
+
+import '../../component/example/bs-highlight';
 
 import 'lit-element-bootstrap/layout/grid';
 import 'lit-element-bootstrap/components/navs';
@@ -12,16 +17,83 @@ class GettingStartedView extends LitElement {
         return [
             BsContentRebootCss,
             BsContentTypographyCss,
-            BsFlexOrderCss
+            BsDocsContentCss,
+            BsFlexOrderCss,
+            PrismDefaultCss,
+            css`
+                :host {
+                    display: block;
+                }
+            `
         ];
     }
     
     render() {
         return html`
             
-            <h1>Getting started</h1>
-                    
+            <h1 class="bd-title" id="content">Getting Started</h1>
+            
+            <br />
+
+            <h3>Introduction</h3>
+            <p>lit-element-bootstrap is an implementation of Bootstrap CSS (v4.1.3) components using <a href="https://lit-element.polymer-project.org/">LitElement</a> with no dependency on jquery.
+                Each component can be used individually and independently of the other components as well as the majority of the CSS code which is exported as css tagged template literals
+                allowing for re-usability when developing new components.
+            </p>
+
+            <br />
+
+            <h3>Installation</h3>
+            <bs-highlight>
+                <pre><code>${unsafeHTML(this._hightlightJavascript(
+                    `npm install lit-element-bootstrap`
+                ))}</code></pre>
+            </bs-highlight>
+
+            <br />
+
+            <h3>How to import</h3>
+            <p>
+                In most cases it is preferabled to only import just the components that you use. 
+                Once the component is loaded in the browser it is registered as a custom element and available to be used via its tag.
+                The name of the component file usually matches the ES6 class and its tag. Take for example the alert component: 
+                The file is named bs-alert.js, it exports the class BsAlert and it's registered tag is &lt;bs-alert&gt;. 
+            </p>
+            <bs-highlight>
+                <pre><code>${unsafeHTML(this._hightlightJavascript(`
+                    // e.g import only bs-alert
+                    import 'lit-element-bootstrap/components/alert/bs-alert';
+
+                    // e.g import all alert components
+                    import 'lit-element-bootstrap/components/alert';
+
+                    // e.g import all components (should be avoided)
+                    import 'lit-element-bootstrap';
+
+                `))}</code></pre>
+            </bs-highlight>
+            
+            <br />
+
+            <h3>Browser Compatibility</h3>
+            <p>
+                Since lit-element-bootstrap is build using LitElement it has the same compatibility. 
+                All components are exported as ES6 classes and re-usable CSS is exported via the <a href="https://lit-element.polymer-project.org/guide/styles">css tagged template literal.</a> 
+            </p>
+
         `;
+    }
+
+    // import 'lit-element-bootstrap/components/alert/';
+
+    _hightlightJavascript(text) {
+
+        const grammar = window.Prism.languages.javascript;
+        const language = 'javascript';
+        const normilazedText = window.Prism.plugins.NormalizeWhitespace.normalize(text);
+        const hightlightedText = window.Prism.highlight(normilazedText, grammar, language);
+
+        return hightlightedText;
     }
 };
 
