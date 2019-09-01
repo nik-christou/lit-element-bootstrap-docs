0fa94b4a7622daa17a3f0ef8306402db1cef7a54
nikolas <nikolas@nikolass-mbp>
2019-09-01 21:58:43 +0300

    Updated home view with new version

--- src/views/home/home-view.js
+++ src/views/home/home-view.js
@@ -53,7 +53,7 @@ class HomeView extends LitElement {
                             <bs-link-button outline-primary large href="/getting-started">Get started</bs-link-button>
                             <bs-link-button outline-secondary large href="https://github.com/nik-christou/lit-element-bootstrap" target="_black">Github</bs-link-button>
                         </div>
-                        <p class="text-muted">Current version: 1.0.0-alpha.15 (31 Aug 19)</p>
+                        <p class="text-muted">Current version: 1.0.0-alpha.16 (01 Sep 19)</p>
                     </bs-jumbotron>
                 </bs-column>
             </bs-row>
