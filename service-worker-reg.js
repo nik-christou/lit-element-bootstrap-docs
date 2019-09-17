f3b6451fa413503105d9ecb21024405004bd85ec
nikolas <nikolas@nikolas-All-Series>
2019-09-17 22:44:54 +0300

    Working on integrating workbox service worker

---
+++ service-worker-reg.js
@@ -0,0 +1,30 @@
+
+import { Workbox } from './web_modules/workbox-window/Workbox.mjs';
+
+if ('serviceWorker' in navigator) {
+    
+    const wb = new Workbox('/service-worker.js');
+    
+    wb.addEventListener('activated', (event) => {
+        
+        // `event.isUpdate` will be true if another version of the service
+        // worker was controlling the page when this version was registered.
+        if (!event.isUpdate) {
+            console.log('Service worker activated for the first time!');
+      
+          // If your service worker is configured to precache assets, those
+          // assets should all be available now.
+        }
+    });
+
+    wb.addEventListener('message', (event) => {
+        if (event.data.type === 'CACHE_UPDATED') {
+          const {updatedURL} = event.data.payload;
+      
+          console.log(`A newer version of ${updatedURL} is available!`);
+        }
+    });
+
+    // Register the service worker after event listeners have been added.
+    wb.register();
+}
---
+++ service-worker.js
@@ -0,0 +1,8 @@
+
+importScripts('./web_modules/workbox-sw.js');
+
+if (workbox) {
+    console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+    console.log(`Boo! Workbox didn't load 😬`);
+}
