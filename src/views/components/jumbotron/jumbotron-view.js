ce995cc46713b06558bbf1507e614f4b5d24c1d3
nikolas <nikolas@nikolass-mbp>
2019-10-13 23:07:20 +0300

    Fixed issue with wrong import

--- src/views/components/jumbotron/jumbotron-view.js
+++ src/views/components/jumbotron/jumbotron-view.js
@@ -2,7 +2,7 @@
 import { LitElement, html } from 'lit-element';
 import { BaseViewMixin } from '../../base-view.js';
 import { unsafeHTML } from 'lit-html/directives/unsafe-html.js';
-import { BsDocsContentCss } from '../../../component/content/bs-docs-content-css.js';
+import { BsDocsContentCss } from '../../../component/content/bs-docs-content.css.js';
 import { BsContentRebootCss, BsContentTypographyCss } from '@lit-element-bootstrap/content';
 
 import './examples/jumbotron-example.js';
